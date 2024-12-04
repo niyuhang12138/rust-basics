b9e626488e470259dab08dac1b247d8caecdafa1
nyh <2830481694@qq.com>
2024-12-04 17:13:25 +0800

    泛型编程

--- 02_基础篇/code/_08_memsiz/src/main.rs
+++ 02_基础篇/code/_08_memsiz/src/main.rs
@@ -1,42 +1,48 @@
-use std::collections::HashMap;
-use std::mem::size_of;
+// use std::collections::HashMap;
+// use std::mem::size_of;
 
-enum E {
-    A(f64),
-    B(HashMap<String, String>),
-    C(Result<Vec<u8>, String>),
-}
+// enum E {
+//     A(f64),
+//     B(HashMap<String, String>),
+//     C(Result<Vec<u8>, String>),
+// }
 
-// 这是一个声明宏, 它会打印各种数据结构本身的带下, 在Option中的大小, 以及在Result中的大小
-macro_rules! show_size {
-    (header) => {
-        println!(
-            "{:<24} {:>4} {} {}",
-            "Type", "T", "Option<T>", "Result<T, io::Error>"
-        );
-        println!("{}", "-".repeat(64));
-    };
-    ($t:ty) => {
-        println!(
-            "{:<24} {:4} {:8} {:12}",
-            stringify!($t),
-            size_of::<$t>(),
-            size_of::<Option<$t>>(),
-            size_of::<Result<$t, std::io::Error>>(),
-        )
-    };
-}
+// // 这是一个声明宏, 它会打印各种数据结构本身的带下, 在Option中的大小, 以及在Result中的大小
+// macro_rules! show_size {
+//     (header) => {
+//         println!(
+//             "{:<24} {:>4} {} {}",
+//             "Type", "T", "Option<T>", "Result<T, io::Error>"
+//         );
+//         println!("{}", "-".repeat(64));
+//     };
+//     ($t:ty) => {
+//         println!(
+//             "{:<24} {:4} {:8} {:12}",
+//             stringify!($t),
+//             size_of::<$t>(),
+//             size_of::<Option<$t>>(),
+//             size_of::<Result<$t, std::io::Error>>(),
+//         )
+//     };
+// }
 
+// fn main() {
+//     show_size!(header);
+//     show_size!(u8);
+//     show_size!(f64);
+//     show_size!(&u8);
+//     show_size!(Box<u8>);
+//     show_size!(&[u8]);
+
+//     show_size!(String);
+//     show_size!(Vec<u8>);
+//     show_size!(HashMap<String, String>);
+//     show_size!(E);
+// }
+
+use std::net::SocketAddr;
 fn main() {
-    show_size!(header);
-    show_size!(u8);
-    show_size!(f64);
-    show_size!(&u8);
-    show_size!(Box<u8>);
-    show_size!(&[u8]);
-
-    show_size!(String);
-    show_size!(Vec<u8>);
-    show_size!(HashMap<String, String>);
-    show_size!(E);
+    let addr = "127.0.0.1:8080".parse::<SocketAddr>().unwrap();
+    println!("addr: {:?}, port: {:?}", addr.ip(), addr.port());
 }

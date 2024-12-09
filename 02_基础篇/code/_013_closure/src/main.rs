b3bafc53e4892062214d149e8f5f9455bc7f04ec
niyuhang <2830481694@qq.com>
2024-12-09 22:10:23 +0800

    closure

--- 02_基础篇/code/_013_closure/src/main.rs
+++ 02_基础篇/code/_013_closure/src/main.rs
@@ -102,36 +102,109 @@
 //     c();
 // }
 
+// fn main() {
+//     let v = vec![0_u8; 1024];
+//     let v1 = vec![0_u8; 1023];
+
+//     // Fn, 不移动所有权
+//     let mut c = |x: u64| v.len() as u64 * x;
+//     // Fn 移动所有权
+//     let mut c1 = move |x: u64| v1.len() as u64 * x;
+
+//     println!("direct call: {}", c(2));
+//     println!("direct call: {}", c1(2));
+
+//     println!("call: {}", call(3, &c));
+//     println!("call: {}", call(3, &c1));
+
+//     println!("call_mut: {}", call_mut(4, &mut c));
+//     println!("call_mut: {}", call_mut(4, &mut c1));
+
+//     println!("call_once: {}", call_once(5, c));
+//     println!("call_once: {}", call_once(5, c1));
+// }
+
+// fn call(arg: u64, c: &impl Fn(u64) -> u64) -> u64 {
+//     c(arg)
+// }
+
+// fn call_mut(arg: u64, c: &mut impl FnMut(u64) -> u64) -> u64 {
+//     c(arg)
+// }
+
+// fn call_once(arg: u64, c: impl FnOnce(u64) -> u64) -> u64 {
+//     c(arg)
+// }
+
+// fn main() {
+//     let name = String::from("Tyr");
+//     let vec = vec!["Rust", "Elixir", "Javascript"];
+//     let v = &vec[..];
+//     let data = (1, 2, 3, 4);
+//     let c = move || {
+//         println!("data: {:?}", data);
+//         println!("v: {:?}, name: {:?}", v, name.clone());
+//     };
+//     c();
+//     // 请问在这里，还能访问 name 么？为什么？
+// }
+
+// fn main() {
+//     let mut s = String::from("abc");
+
+//     let mut c = || s.push_str("!");
+
+//     call_mut(&mut c);
+//     call_mut(&mut c);
+
+//     call_once(c);
+
+//     println!("s: {:?}", s);
+// }
+
+// fn call_mut(mut f: impl FnMut()) {
+//     f();
+// }
+
+// fn call_once(f: impl FnOnce()) {
+//     f();
+// }
+
+pub trait Executor {
+    fn execute(&self, cmd: &str) -> Result<String, &'static str>;
+}
+
+impl<F> Executor for F
+where
+    F: Fn(&str) -> Result<String, &'static str>,
+{
+    fn execute(&self, cmd: &str) -> Result<String, &'static str> {
+        self(cmd)
+    }
+}
+
+struct BashExecutor {
+    env: String,
+}
+impl Executor for BashExecutor {
+    fn execute(&self, cmd: &str) -> Result<String, &'static str> {
+        Ok(format!(
+            "fake bash execute: env: {}, cmd: {}",
+            self.env, cmd
+        ))
+    }
+}
+// 看看我给的 tonic 的例子，想想怎么实现让 27 行可以正常执行
 fn main() {
-    let v = vec![0_u8; 1024];
-    let v1 = vec![0_u8; 1023];
-
-    // Fn, 不移动所有权
-    let mut c = |x: u64| v.len() as u64 * x;
-    // Fn 移动所有权
-    let mut c1 = move |x: u64| v1.len() as u64 * x;
-
-    println!("direct call: {}", c(2));
-    println!("direct call: {}", c1(2));
-
-    println!("call: {}", call(3, &c));
-    println!("call: {}", call(3, &c1));
-
-    println!("call_mut: {}", call_mut(4, &mut c));
-    println!("call_mut: {}", call_mut(4, &mut c1));
-
-    println!("call_once: {}", call_once(5, c));
-    println!("call_once: {}", call_once(5, c1));
+    let env = "PATH=/usr/bin".to_string();
+    let cmd = "cat /etc/passwd";
+    let r1 = execute(cmd, BashExecutor { env: env.clone() });
+    println!("{:?}", r1);
+    let r2 = execute(cmd, |cmd: &str| {
+        Ok(format!("fake fish execute: env: {}, cmd: {}", env, cmd))
+    });
+    println!("{:?}", r2);
 }
-
-fn call(arg: u64, c: &impl Fn(u64) -> u64) -> u64 {
-    c(arg)
-}
-
-fn call_mut(arg: u64, c: &mut impl FnMut(u64) -> u64) -> u64 {
-    c(arg)
-}
-
-fn call_once(arg: u64, c: impl FnOnce(u64) -> u64) -> u64 {
-    c(arg)
+fn execute(cmd: &str, exec: impl Executor) -> Result<String, &'static str> {
+    exec.execute(cmd)
 }
